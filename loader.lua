c86b7cdac25a42b7ac056cebb5dbc27b1727a027
6hug <realbhug@gmail.com>
2026-07-18 21:53:33 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,14 @@
-if not game:IsLoaded() then
-    game.Loaded:Wait()
+local function main()
+    loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/be0191b975fb1e9f0705fc2312224e1ff380518d8784419e0cc6ceae783807e1.lua"))()
 end
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/be0191b975fb1e9f0705fc2312224e1ff380518d8784419e0cc6ceae783807e1.lua"))()
+if game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.InProgress then
+        if queue_on_teleport then
+            queue_on_teleport([[
+                script_key = "]] .. script_key .. [["
+                loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/be0191b975fb1e9f0705fc2312224e1ff380518d8784419e0cc6ceae783807e1.lua"))()
+            ]])
+        end
+    end
+end) then end
+main()
