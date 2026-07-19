20290565f6704c216f11a28ebc40ced97a994c1c
6hug <realbhug@gmail.com>
2026-07-19 09:31:16 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,7 +6,7 @@ if game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
         if queue_on_teleport then
             queue_on_teleport([[
                 script_key = "]] .. script_key .. [["
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/6hug/Aurora/refs/heads/main/loader.lua"))()"))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/6hug/Aurora/refs/heads/main/loader.lua"))()
             ]])
         end
     end
