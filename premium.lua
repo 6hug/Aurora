4e84f5be8a044bc7a20e27eeaa006331b7843501
6hug <realbhug@gmail.com>
2026-09-15 22:52:58 +0300

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/36ffca6b1526a6dc6aaf161bd4df67f0.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2b54013dbb37bb2a0f973e0af67871aa.lua"))()
