aa287fedaa44abf650e9e4690c59c032f82f38a1
6hug <realbhug@gmail.com>
2026-09-15 22:53:17 +0300

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/0261059e774f32a7cd84fcd64f18de59.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1cc7d71a363bf715da5183fcf041746d.lua"))()
